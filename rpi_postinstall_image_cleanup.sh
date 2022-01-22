27dfdcf5f58cc2853efb5a61b039bf0a45f6f8e9
Brian Boellner <docter60@hotmail.com>
2022-01-22 16:59:49 -0500

    Add fixes

--- rpi_postinstall_image_cleanup.sh
+++ rpi_postinstall_image_cleanup.sh
@@ -31,19 +31,21 @@ rm /vagrant/irix/6.5.22/Overlay/disc1/disc1.tar.gz
 rm /vagrant/irix/6.5.22/Overlay/disc2/disc2.tar.gz
 rm /vagrant/irix/6.5.22/Overlay/disc3/disc3.tar.gz
 rm /vagrant/irix/6.5.22/Overlay/apps/apps.tar.gz
-rm /vagrant/irix/Development/devlibs/developmentlibraries.tar.gz
-rm /vagrant/irix/Development/mipspro_ap/mipsproap.tar.gz
-rm /vagrant/irix/Development/mipspro_c/mipspro_c.tar.gz
-rm /vagrant/irix/Development/mipspro_cee/mipspro_cee.tar.gz
-rm /vagrant/irix/Development/mipspro_cpp/mipspro_cpp.tar.gz
-rm /vagrant/irix/Development/mipspro_update/mipspro744update.tar.gz
-rm /vagrant/irix/Development/mipspro/mipspro-7.4.3m.tar
-rm /vagrant/irix/Development/prodev/prodev.tar.gz
-rm /vagrant/irix/Extras/sgifonts/sgipostscriptfonts.tar.gz
-rm /vagrant/irix/Extras/perfcopilot/perfcopilot.tar.gz
-rm /vagrant/irix/Foundation/disc1/foundation1.tar.gz
-rm /vagrant/irix/Foundation/disc2/foundation2.tar.gz
-rm /vagrant/irix/Foundation/nfs/onc3nfs.tar.gz
+rm /vagrant/irix/6.5/Development/devlibs/developmentlibraries.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro_ap/mipsproap.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro_c/mipspro_c.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro_cee/mipspro_cee.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro_cpp/mipspro_cpp.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro_update/mipspro744update.tar.gz
+rm /vagrant/irix/6.5/Development/mipspro/mipspro-7.4.3m.tar
+rm /vagrant/irix/6.5/Development/prodev/prodev.tar.gz
+rm /vagrant/irix/6.5/Extras/sgifonts/sgipostscriptfonts.tar.gz
+rm /vagrant/irix/6.5/Extras/perfcopilot/perfcopilot.tar.gz
+rm /vagrant/irix/6.5/Foundation/disc1/foundation1.tar.gz
+rm /vagrant/irix/6.5/Foundation/disc2/foundation2.tar.gz
+rm /vagrant/irix/6.5/Foundation/nfs/onc3nfs.tar.gz
+rm /vagrant/irix/5.3/Development/devfoundations/irix_5_3_development.tgz
+rm /vagrant/irix/5.3/Foundation/disc1/irix_5_3_foundation.tgz
 
 echo "Zeroing free space. This will take a while."
 dd if=/dev/zero of=/EMPTY bs=1M 
