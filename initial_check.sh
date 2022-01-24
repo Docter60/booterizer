afe93ddaf2c78f0ee7e3c2b60e95d77be3b215d6
Brian Boellner <docter60@hotmail.com>
2022-01-23 22:07:12 -0500

    Polish initial_check.sh

--- initial_check.sh
+++ initial_check.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 apt update && apt upgrade
-apt install git sshpass ansible python3-pip
-mkdir /home/pi/.ssh
+apt install git sshpass python3-pip
+pip3 install -r $(dirname $(realpath "$0"))/ansible/requirements.txt
+install -d -m 0764 -o pi -g pi /home/pi/.ssh
+install -m 0764 -o pi -g pi /dev/null /home/pi/.ssh/known_hosts
 ssh-keyscan -H localhost >> /home/pi/.ssh/known_hosts
